f3899e5e44241f6dd04387fe1babb8dc9685740a
shanecss <huqing758@163.com>
2020-03-27 15:35:46 +0800

    add scss file

---
+++ src/renderer/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Login">
+        {{msg}}
+    </div>
+</template>
+<script>
+export default {
+    name:'Login',
+    data(){
+        return{
+            msg:'登录页面'
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -4,15 +4,12 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
-    },
-    {
-      path: '*',
-      redirect: '/'
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'Login',
+        component: require('@/components/Login').default
+    },{
+        path: '*',
+        redirect: '/'
+    }]
 })
